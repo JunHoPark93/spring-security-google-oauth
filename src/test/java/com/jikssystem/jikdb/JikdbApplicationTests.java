1ef3e4ae96f6c921cf3daf566ca4ab2175491eac
pjh374968@gmail.com <pjh374968@gmail.com>
2018-11-27 23:21:14 +0900

    Create rest test to jikdb application

--- src/test/java/com/jikssystem/jikdb/JikdbApplicationTests.java
+++ src/test/java/com/jikssystem/jikdb/JikdbApplicationTests.java
@@ -1,16 +1,34 @@
 package com.jikssystem.jikdb;
 
+import io.restassured.RestAssured;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JikdbApplicationTests {
 
+    @Before
+    public void setup() {
+        RestAssured.port = 8080;
+    }
+
     @Test
-    public void contextLoads() {
+    public void JikDbMainCall() {
+
+        given()
+                .when()
+                .get("/")
+                .then()
+                .statusCode(200)
+                .contentType("text/html")
+                .body(containsString("권한 관리"));
     }
 
 }
