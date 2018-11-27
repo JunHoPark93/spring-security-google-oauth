5963ea9b148919bf9fb6654def6f181e887b5a67
pjh374968@gmail.com <pjh374968@gmail.com>
2018-11-27 22:34:00 +0900

    Initial commit

---
+++ src/main/java/com/jikssystem/jikdb/JikdbApplication.java
@@ -0,0 +1,12 @@
+package com.jikssystem.jikdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JikdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JikdbApplication.class, args);
+    }
+}
