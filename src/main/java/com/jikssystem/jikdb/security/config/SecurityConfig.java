71f70f9eaa12190375f08ee1bf9ab3ce2a55f4d0
pjh374968@gmail.com <pjh374968@gmail.com>
2018-11-28 23:51:32 +0900

    Create OAuthConfig, Add filter to SecurityConfig

--- src/main/java/com/jikssystem/jikdb/security/config/SecurityConfig.java
+++ src/main/java/com/jikssystem/jikdb/security/config/SecurityConfig.java
@@ -4,20 +4,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final Filter ssoFilter;
+
+    public SecurityConfig(Filter ssoFilter) {
+        this.ssoFilter = ssoFilter;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/**")
                 .authorizeRequests()
-                .antMatchers("/", "/h2-console/**", "/favicon.ico").permitAll()
+                .antMatchers("/", "/h2-console/**", "/favicon.ico", "/login**").permitAll()
                 .anyRequest().authenticated()
                 .and().logout().logoutSuccessUrl("/").permitAll()
                 .and().headers().frameOptions().sameOrigin()
-                .and().csrf().disable();
+                .and().csrf().disable()
+                .addFilterBefore(ssoFilter, BasicAuthenticationFilter.class); // ssoFilter 추가
 
         // root와 h2-console (test용), favicon은 권한 부여하지 않음
         // csrf 비활성화
