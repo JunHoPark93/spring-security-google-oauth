7ee39052117bb12a43bad09fd7e00b4e186cd915
pjh374968@gmail.com <pjh374968@gmail.com>
2018-11-30 11:56:28 +0900

    Create session configs, app config

---
+++ src/main/java/com/jikssystem/jikdb/security/config/HttpSessionConfig.java
@@ -0,0 +1,7 @@
+package com.jikssystem.jikdb.security.config;
+
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+@EnableJdbcHttpSession
+public class HttpSessionConfig {
+}
---
+++ src/main/java/com/jikssystem/jikdb/security/config/SessionConstants.java
@@ -0,0 +1,5 @@
+package com.jikssystem.jikdb.security.config;
+
+public interface SessionConstants {
+    String LOGIN_USER = "LOGIN_USER";
+}
